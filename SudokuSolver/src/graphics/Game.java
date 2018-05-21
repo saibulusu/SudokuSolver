0b254cd299dba6c8a61f668376f452cd7d6f6d42
Sai Bulusu <sai.bulusu@gmail.com>
2018-05-21 10:55:21 -0700

    Working on graphics implementation and "solve" button

--- SudokuSolver/src/graphics/Game.java
+++ SudokuSolver/src/graphics/Game.java
@@ -12,8 +12,6 @@ import java.util.Scanner;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-import console.Puzzle;
-
 // graphics implementation of the Sudoku puzzle
 public class Game {
 
@@ -39,29 +37,60 @@ public class Game {
 
 		frame.setLocation(480, 180);
 
-		frame.setLayout(new GridLayout(9, 9));
+		frame.setLayout(new GridLayout(10, 9));
+		
+		JButton[][] buttons = new JButton[9][9];
 
-		for (int r = 0; r < 9; r++) {
-			for (int c = 0; c < 9; c++) {
-				JButton button = new JButton();
-				button.setFont(new Font("Arial", Font.PLAIN, 20));
-				if (puzzle.board[r][c] != 0) {
-					button.setText(board[r][c] + "");
-					button.setForeground(Color.BLUE);
-				}
-				button.setPreferredSize(new Dimension(60, 60));
-				button.addActionListener(new ActionListener() {
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						
+		for (int r = 0; r < 10; r++) {
+			if (r == 9) {
+				for (int c = 0; c < 9; c++) {
+					JButton button = new JButton();
+					button.setFont(new Font("Arial", Font.PLAIN, 20));
+//					button.setText("SOLVE");
+					
+					frame.add(button);
+					
+					if (c != 4) {
+						button.setOpaque(false);
+						button.setContentAreaFilled(false);
+						button.setBorderPainted(false);
+					} else {
+						button.setText("SOLVE");
+						button.addActionListener(new ActionListener() {
+							@Override
+							public void actionPerformed(ActionEvent e) {
+								// TODO Auto-generated method stub
+								puzzle.solve(buttons);
+							}
+						});
 					}
-				});
-				frame.add(button);
+					frame.add(button);
+				}
+			} else {
+				for (int c = 0; c < 9; c++) {
+					JButton button = new JButton();
+					buttons[r][c] = button;
+					button.setFont(new Font("Arial", Font.PLAIN, 20));
+					if (puzzle.board[r][c] != 0) {
+						button.setText(board[r][c] + "");
+						button.setForeground(Color.BLUE);
+					}
+					button.setPreferredSize(new Dimension(60, 60));
+					button.addActionListener(new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							
+						}
+					});
+					frame.add(button);
+				}
+
+				frame.pack();
+
 			}
-
-			frame.pack();
 		}
-
+		
+//		puzzle.solve(buttons);
 	}
 	
 }
---
+++ SudokuSolver/src/graphics/Puzzle.java
@@ -0,0 +1,117 @@
+package graphics;
+
+import java.awt.Color;
+
+import javax.swing.JButton;
+
+public class Puzzle {
+
+	public int[][] board;
+	
+	public Puzzle(int[][] board) {
+		this.board = board;
+	}
+	
+	public String toString() {
+		String res = "";
+		for (int r = 0; r < 9; r++) {
+			for (int c = 0; c < 9; c++) {
+				res += board[r][c] + " ";
+			}
+			res += "\n";
+		}
+		return res;
+	}
+	
+	// check if the puzzle is already solved
+	boolean solved() {
+		for (int r = 0; r < 9; r++) {
+			for (int c = 0; c < 9; c++) {
+				if (board[r][c] == 0) {
+					// if there is a 0 anywhere in the board, then it is not yet solved
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	// analyze a given position in the puzzle to see which values can be placed there
+	boolean[] analyze(int R, int C) {
+		// possible[i] being true means that i is a possible value for the position at (R,C)
+		boolean[] possible = new boolean[10];
+		
+		// automatically every value is possible but later remove those that are not possible
+		for (int i = 0; i < possible.length; i++) {
+			possible[i] = true;
+		}
+		
+		// for every row with the same column index, analyze
+		for (int r = 0; r < 9; r++) {
+			if (board[r][C] != 0) {
+				possible[board[r][C]] = false;
+			}
+		}
+		
+		// for every column with the same row index, analyze
+		for (int c = 0; c < 9; c++) {
+			if (board[R][c] != 0) {
+				possible[board[R][c]] = false;
+			}
+		}
+		
+		// for the surrounding box, analyze
+		int startR = R / 3 * 3;
+		int startC = C / 3 * 3;
+		
+		for (int r = startR; r < startR + 3; r++) {
+			for (int c = startC; c < startC + 3; c++) {
+				if (board[r][c] != 0) {
+					possible[board[r][c]] = false;
+				}
+			}
+		}
+		
+		return possible;
+	}
+	
+	// solve the board recursively
+	public void solve(JButton[][] buttons) {
+		if (solved()) {
+			// base case: the board is already solved
+			System.out.println("SOLVED");
+			System.out.println(this);
+			return;
+		} else {
+			// recursive step: analyze each position with 0 and check every possible value
+			int R = -1;
+			int C = -1;
+			
+			// find the first position with 0 at its value
+			for (int r = 0; r < 9; r++) {
+				for (int c = 0; c < 9; c++) {
+					if (board[r][c] == 0 && R == -1) {
+						R = r;
+						C = c;
+					}
+				}
+			}
+			
+			// possible holds all of the values that can be placed there
+			boolean[] possible = analyze(R, C);
+			
+			// for every value that can be placed at this position, place it and solve
+			for (int i = 1; i < 10; i++) {
+				if (possible[i]) {
+					board[R][C] = i;
+					buttons[R][C].setText(i + "");
+					buttons[R][C].setForeground(Color.BLACK);
+					solve(buttons);
+				}
+			}
+			
+			// if all else fails, then set the value to 0, so the program will backtrack and remove a value
+			board[R][C] = 0;
+		}
+	}
+}
